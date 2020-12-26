fdec3c9325318ff9fdea7b9c6c1ac626926a8c51
CodYanJS <earthlumier0@gmail.com>
2020-12-26 10:26:56 +0100

    Initial Commit

---
+++ Laiko/LaikoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaikoApp.swift
+//  Laiko
+//
+//  Created by LuMier0 on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct LaikoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
