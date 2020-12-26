37dc62dc9b51bb0a00ccada9fcb36918c697cbf7
CodYanJS <earthlumier0@gmail.com>
2020-12-26 16:30:38 +0100

    Build en prévision de la pré-bêta 0.0.1-A commit 00A01A

--- Laiko/ContentView.swift
+++ Laiko/ContentView.swift
@@ -11,16 +11,19 @@ struct ContentView: View {
     var body: some View {
         VStack {
             LogoView()
-                .padding(.horizontal, -150)
+                .padding(.horizontal, -180)
             VStack (alignment: .trailing){
             
             Text("It's work !")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .padding(.vertical, -100)
             HStack {
                 Text("Developer release")
                     .font(.subheadline)
+                    .padding(.vertical, -75)
                 Text("0.0.1-A")
                     .font(.subheadline)
+                    .padding(.vertical, -75)
             }
         }
             .padding()
