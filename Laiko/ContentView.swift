3a08b71c72f9796d43f0eb5a55641f8c5eadeb7a
CodYanJS <earthlumier0@gmail.com>
2020-12-26 10:49:56 +0100

    First Commit

--- Laiko/ContentView.swift
+++ Laiko/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("It's work !, Developer release 0.0.1")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .multilineTextAlignment(.center)
+            .lineLimit(3)
+            .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+            .frame(width: nil)
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewLayout(.device)
+                .previewDevice("iPhone 12 Pro")
+                .previewDisplayName("Iphone 12 Pro")
+            ContentView()
+        }
     }
 }
